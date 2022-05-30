4a708b88ac48fe163ee83ee0fd8915a82de0ead9
Dani <60989767+Visalba@users.noreply.github.com>
2022-05-30 22:01:55 +0200

    Add files via upload

--- Fratres/Fratres_MIDI.py
+++ Fratres/Fratres_MIDI.py
@@ -26,7 +26,6 @@ def getTvoice(m,t):
     return vocs[ind]+oct*7
 # print(getTvoice(4,1))
 # test according to table of https://aestheticcomplexity.wordpress.com/2014/03/23/mapping-tintinnabuli-transformations/
-
 l = 'abcdefg'   
 for n in range(-14,-7):  # range(7)
     t = getTvoice(n,-3)
@@ -57,7 +56,7 @@ root_note = 4
 for j in range(0,9):
     ini = [expand(4-3*j,i) for i in range(2,5)]  # primeras 3 expansiones, 3 compases
     fratres.extend(ini + [m[::-1] for m in ini])   # añadimos los compases invertidos
-
+    
 durs = [[2] + [1]*(len(c)-2) +[2] for c in fratres]  # generacion de figuras/duraciones
 
 
@@ -67,27 +66,14 @@ print(durs)
 f = [x for sublist in fratres for x in sublist]
 d = [x for sublist in durs for x in sublist]
 t = tint(f)
+
 print('f ', f)
 print('t ', t)
 print('d ', d)
 
-Clock.bpm=80
-p1 >> keys(f, dur=d, room=.8, mix=0.4, spin=2, amp=0.8)
-
-p2 >> bell(t, dur=d, delay=0.01, room=.8, mix = 0.4, spin=7, amp=0.4)
 
 
-p2 >> glass(
-    dur=1,
-    amp=PWhite(0.02,0.05),
-    #pan=linvar([-0.8,0.8],2),
-    spin=2,
-    hpf=linvar([400,1200],4), hpr=0.8,
-    oct=6,
-    ) + (0,P[2,4])
-p3 >> spaces(
-    amp=0.1,    
-    pan=linvar([-.8,.8],4),
-    hpf=linvar([400,1200],4), hpr=0.8,
-    room=0.4,mix=0.3,
-    oct=3) + (0,2,4)
+Clock.bpm=60
+Clock.set_time(-1)
+p1 >> MidiOut(f, dur=d, channel = 1)
+c1 >> MidiOut(t, dur=d, channel = 2)
